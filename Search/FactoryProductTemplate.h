f879c0d82728a6915b55444b20f1e1f7184614c7
yxtj <yanxiangtianji@gmail.com>
2016-07-11 03:04:53 -0400

    compiling compatible

--- Search/FactoryProductTemplate.h
+++ Search/FactoryProductTemplate.h
@@ -7,8 +7,9 @@ class FactoryProductTemplate
 public:
 	virtual bool parse(const std::vector<std::string>& param) = 0;
 	virtual ~FactoryProductTemplate() = default;
-	void checkParam(const std::vector<std::string>& param, int reqired, const std::string& name)
-		noexcept(false); // throw an exception if fails
+
+	// throw an exception if fails
+	void checkParam(const std::vector<std::string>& param, int reqired, const std::string& name);
 private:
 	void checkNumber(int required, size_t given);
 	void checkName(const std::vector<std::string>& param, const std::string& name);
