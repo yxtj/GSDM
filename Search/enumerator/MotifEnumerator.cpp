61abb8313438158fb255521e6062002341590c4d
yxtj <yanxiangtianji@gmail.com>
2016-10-19 12:45:22 -0400

    add basic interface for MotifEnumerator

--- Search/enumerator/MotifEnumerator.cpp
+++ Search/enumerator/MotifEnumerator.cpp
@@ -16,6 +16,27 @@ MotifEnumerator::~MotifEnumerator()
 {
 }
 
+bool MotifEnumerator::parse(const std::vector<std::string>& param)
+{
+	return true;
+}
+
+void MotifEnumerator::setStartPoint(const Motif & m)
+{
+}
+
+void MotifEnumerator::setEndPoint(const Motif & m)
+{
+}
+
+void MotifEnumerator::setStartPoint(const int shardID)
+{
+}
+
+void MotifEnumerator::setEndPoint(const int shardID)
+{
+}
+
 int MotifEnumerator::nEdgeNext() const
 {
 	return 0;
@@ -24,4 +45,24 @@ int MotifEnumerator::nEdgeNext() const
 int MotifEnumerator::nNodeNext() const
 {
 	return 0;
+}
+
+bool MotifEnumerator::isStable() const
+{
+	return false;
+}
+
+bool MotifEnumerator::isParallelizable() const
+{
+	return false;
+}
+
+bool MotifEnumerator::isUniqueWhenPara() const
+{
+	return false;
+}
+
+bool MotifEnumerator::isIndependentWhenPara() const
+{
+	return false;
 }
--- Search/enumerator/MotifEnumerator.h
+++ Search/enumerator/MotifEnumerator.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "../../common/Motif.h"
+#include <string>
+#include <vector>
 
 class MotifEnumerator
 {
@@ -9,9 +11,26 @@ public:
 	MotifEnumerator(const int n);
 	virtual ~MotifEnumerator();
 
+	virtual bool parse(const std::vector<std::string>& param);
+
+	// start from the first motif, end when everything is enumerated
+	virtual void setStartPoint() = 0;
+	virtual void setEndPoint() = 0;
+	// start from given motif, end before the given motif
+	virtual void setStartPoint(const Motif& m);
+	virtual void setEndPoint(const Motif& m);
+	// start as the given part, used for the case given total number of shards
+	virtual void setStartPoint(const int shardID);
+	virtual void setEndPoint(const int shardID);
+
 	virtual bool hasNext()const = 0;
 	virtual Motif next() = 0;
 	virtual int nEdgeNext() const;
 	virtual int nNodeNext() const;
+
+	virtual bool isStable() const;
+	virtual bool isParallelizable() const;
+	virtual bool isUniqueWhenPara() const;
+	virtual bool isIndependentWhenPara() const;
 };
 
