5aa0fcf7669e3aac74f77506f3d3ffca593852a8
yxtj <yanxiangtianji@gmail.com>
2016-11-04 00:08:06 -0400

    add grouped figures for alpha

--- SupportScript/plot/plot-smy.m
+++ SupportScript/plot/plot-smy.m
@@ -14,14 +14,15 @@ fontSize=18;
 printSize=[0 0 4 3]*1.5;
 close all
 for iw=1:numel(WINDOWS); for ig=1:numel(GRAPHS); for ifn=1:numel(FILENAMES);
-	fn=[WINDOWS{iw} '/' GRAPHS{ig} '/' FILENAMES{ifn}]
-	data=smyLoader([dataPrefix fn]);
-	drawRawPRA('minSup','ms',data.minsup,'T',data.theta,'A',data.alpha,data,figPrefix,fn,type,fontSize,printSize)
+	fnf=[WINDOWS{iw} '/' GRAPHS{ig} '/' FILENAMES{ifn}]
+	data=smyLoader([dataPrefix fnf]);
+	drawRawPRA('minSup','ms',data.minsup,'T',data.theta,'A',data.alpha,data,figPrefix,fnf,type,fontSize,printSize)
     close all;
-    drawRawPRA('theta','th',data.theta,'M',data.minsup,'A',data.alpha,data,figPrefix,fn,type,fontSize,printSize)
+    drawRawPRA('theta','th',data.theta,'M',data.minsup,'A',data.alpha,data,figPrefix,fnf,type,fontSize,printSize)
 	close all;
 end;end;end;
 
+% draw figures for th-recall with legend
 for iw=1:numel(WINDOWS); for ig=1:numel(GRAPHS); for ifn=1:numel(FILENAMES);
     fn=[WINDOWS{iw} '/' GRAPHS{ig} '/' FILENAMES{ifn}]
 	data=smyLoader([dataPrefix fn]);
@@ -32,19 +33,34 @@ for iw=1:numel(WINDOWS); for ig=1:numel(GRAPHS); for ifn=1:numel(FILENAMES);
     addLegendOutside(keys,'NorthEastOutside');
     if ~strcmp(type,'none')
         set(gcf,'PaperUnits','inches','PaperPosition',printSize);
-        fn=strrep(strrep(fn,'.tsv',''),'tbl-','');
-        fn=strrep(strrep(fn, '0.', ''),'/','_');
-        saveas(gcf,[figPrefix '/' fn '_th_reca-lgd.eps'],'epsc')
+        fnf=strrep(strrep(fn,'.tsv',''),'tbl-','');
+        fnf=strrep(strrep(fnf, '0.', ''),'/','_');
+        saveas(gcf,[figPrefix '/' fnf '_th_reca-lgd.eps'],'epsc')
     end
 end;end;end;
 close all;
 
+% draw figure for 
+u=unique(data.minsup);
+for i=1:4
+    subplot(2,2,i);
+    ms=u(i);
+    idx=find(data.minsup==ms);
+    keys=drawRawGroupOne(data.theta(idx),'M',data.minsup(idx),'A',data.alpha(idx),data.recall(idx));
+    %legend(keys,'location','northeast');
+    columnlegend(2,keys,'location','northeast');
+end
+fnf=strrep(strrep(fn,'.tsv',''),'tbl-','');
+fnf=strrep(strrep(fnf, '0.', ''),'/','_');
+saveas(gcf,[figPrefix '/' fnf '_th_reca-group-al' '.eps'],'epsc')
+
+% -----------------
 % draw a summary figure of all graph configurations
 % matlab put lower dimension first
 labels=cell(numel(FILENAMES),numel(GRAPHS),numel(WINDOWS));
 for iw=1:numel(WINDOWS); for ig=1:numel(GRAPHS); for ifn=1:numel(FILENAMES);
-	fn=[WINDOWS{iw} '/' GRAPHS{ig} '/' FILENAMES{ifn}]
-	data=smyLoader([dataPrefix fn]);
+	fnf=[WINDOWS{iw} '/' GRAPHS{ig} '/' FILENAMES{ifn}]
+	data=smyLoader([dataPrefix fnf]);
     datas(ifn,ig,iw)=data;
     s=[WINDOWS{iw} '-' GRAPHS{ig}];
     labels(ifn,ig,iw)=mat2cell(s,1,length(s));
@@ -63,9 +79,10 @@ close all
 keys=drawEBGroupOne(datas,labels,'theta', 'alpha', 0.5,'recall');
 keys=drawEBGroupOne(datas,labels,'theta', 'alpha', 1.5,'recall');
 set(gca,'FontSize', 20)
-ylim([0 1]);xlabel(lblX); ylabel('recall')
+ylim([0 1]);xlabel('theta'); ylabel('recall')
 columnlegend(2,keys,'Location','SouthEast');
 set(gcf,'PaperUnits','inches','PaperPosition',[0 0 6 4.5])
 saveas(gcf,[figPrefix 'smy-alpha-0_5_th_reca.png'],'png')
 saveas(gcf,[figPrefix 'smy-alpha-0_5_th_reca.eps'],'epsc')
+
 
