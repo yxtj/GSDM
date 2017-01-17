f8f8c32435921fe9272fa4238e9794ead6918e50
yxtj <yanxiangtianji@gmail.com>
2017-01-16 22:26:42 -0500

    add a type_traits: is_pair

---
+++ util/is_pair.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <type_traits>
+#include <utility>
+#include "type_trais_dummy.h"
+
+template<typename T, typename Enable = void>
+struct is_pair: public std::false_type {};
+
+template <typename T>
+struct is_pair<
+	T,
+	typename std::conditional<
+		false,
+		impl::type_traits::dummy_t<
+			typename T::first_type,
+			typename T::second_type
+	>,
+	void
+	>::type
+> : public std::true_type{};
