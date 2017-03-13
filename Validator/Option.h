0af8683140837d2707bf6c73b086ccff8bd4aa7d
yxtj <yanxiangtianji@gmail.com>
2017-03-13 16:30:36 -0400

    use holder and objfunc library

--- Validator/Option.h
+++ Validator/Option.h
@@ -6,20 +6,20 @@ class Option
 {
 	boost::program_options::options_description desc;
 public:
+	bool show;
 
 	int nNode;//number of nodes;
 	int nPosInd, nNegInd;//number of +/- individuals
 	int nSnapshot;//number of snapshots for each individual
 	std::vector<int> typePos, typeNeg;//type of the positive and negative individual
-
-	std::vector<int> blacklist;
-
-	bool holdAllData; //for distributed case, whether to make all workers hold all the data
-	bool graphFolderShared; //for distributed case, when each worker holds different part of data:
-							//  whether the input data folder contains all the graphs
-	std::string prefix, graphFolder;//data folder prefix and graph sub-folder
-
 	double theta;
+	bool periodic; //whether to check the periodic occurrence
+	
+	std::string graphFolder; //graph folder
+	//bool holdAllData; //for distributed case, whether to make all workers hold all the data
+	//bool graphFolderShared; //for distributed case, when each worker holds different part of data:
+							//  whether the input data folder contains all the graphs
+	
 	std::string funName;
 	double alpha;
 public:
@@ -32,6 +32,5 @@ public:
 
 private:
 	static std::string& sortUpPath(std::string & path);
-	std::string& processSubPath(std::string& path);
 };
 
--- Validator/Scorer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Scorer
-{
-public:
-	static double diff(const double fp, const double fn, const double alpha);
-	static double ratio(const double fp, const double fn);
-};
-
